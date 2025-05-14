bae3a5e45f52b16034ce8f09bae037b3a6f33976
Raichu15678 <matefekete355@gmail.com>
2025-05-14 10:39:23 +0200

    remove unused fetch and rendering logic for muzeumok

--- js/alap.js
+++ js/alap.js
@@ -1,11 +1,3 @@
-fetch("https://openplzapi.org/li/Communes")
-.then(x=>x.json())
-.then(y=>megjelenit(y))
-
-function megjelenit(y) {
-    console.log(y)
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const latvanyossagokLista = document.getElementById("latvanyossagok-lista");
   latvanyossagok.forEach(latvanyossag => {
@@ -21,23 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
       </div>
     `;
   });
-
-  const muzeumokLista = document.getElementById("muzeumok-lista");
-  muzeumok.forEach(muzeum => {
-    muzeumokLista.innerHTML += `
-      <div class="col-md-6">
-        <div class="card mb-3">
-          <img src="images/${muzeum.kep}" class="card-img-top" alt="${muzeum.nev}">
-          <div class="card-body">
-            <h5 class="card-title">${muzeum.nev}</h5>
-            <p class="card-text">Típus: ${muzeum.tipus}</p>
-            <p class="card-text">Műtárgyak száma: ${muzeum.mutargyakSzama}</p>
-          </div>
-        </div>
-      </div>
-    `;
-  });
-});
 
 
 
