f5cfa5d29de66e1ff90d559f27e92a43ccdba135
Raichu15678 <matefekete355@gmail.com>
2025-05-14 12:07:03 +0200

    add: implement rendering logic for latvanyossagok

---
+++ js/alap.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const latvanyossagokLista = document.getElementById("latvanyossagok-lista");
+  latvanyossagok.forEach(latvanyossag => {
+    latvanyossagokLista.innerHTML += `
+      <div class="col-md-6">
+        <div class="card mb-3">
+          <img src="images/${latvanyossag.kep}" class="card-img-top" alt="${latvanyossag.nev}">
+          <div class="card-body">
+            <h5 class="card-title">${latvanyossag.nev}</h5>
+            <p class="card-text">${latvanyossag.leiras}</p>
+          </div>
+        </div>
+      </div>
+    `});
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
